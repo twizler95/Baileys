367ac99993248cb3104fdb70a39a2870de6bb95c
Nick (M2) <nicktee89@gmail.com>
2025-04-17 15:33:34 +0800

    Update link-preview.ts
    test update preview link

--- src/Utils/link-preview.ts
+++ src/Utils/link-preview.ts
@@ -1,4 +1,4 @@
-import { AxiosRequestConfig } from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 import { Logger } from 'pino'
 import { WAMediaUploadFunction, WAUrlInfo } from '../Types'
 import { prepareWAMessageMedia } from './messages'
@@ -46,12 +46,66 @@ export const getUrlInfo = async(
 		const retries = 0
 		const maxRetry = 5
 
-		const { getLinkPreview } = await import('link-preview-js')
+		const { getLinkPreview, getPreviewFromContent } = await import('link-preview-js')
+		
 		let previewLink = text
 		if(!text.startsWith('https://') && !text.startsWith('http://')) {
 			previewLink = 'https://' + previewLink
 		}
 
+		const VALID_URL_REGEX = new RegExp(
+			"^" +
+			  // protocol identifier
+			  "(?:(?:https?|ftp)://)" +
+			  // user:pass authentication
+			  "(?:\\S+(?::\\S*)?@)?" +
+			  "(?:" +
+			  // IP address exclusion
+			  // private & local networks
+			  "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +
+			  "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +
+			  "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
+			  // IP address dotted notation octets
+			  // excludes loopback network 0.0.0.0
+			  // excludes reserved space >= 224.0.0.0
+			  // excludes network & broacast addresses
+			  // (first & last IP address of each class)
+			  "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
+			  "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
+			  "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
+			  "|" +
+			  // host name
+			  "(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)" +
+			  // domain name
+			  "(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*" +
+			  // TLD identifier
+			  "(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))" +
+			  // TLD may end with dot
+			  "\\.?" +
+			  ")" +
+			  // port number
+			  "(?::\\d{2,5})?" +
+			  // resource path
+			  "(?:[/?#]\\S*)?" +
+			  "$",
+			"i"
+		  );
+
+		const detectedUrl = previewLink
+			.replace(/\n/g, ` `)
+			.split(` `)
+			.find((token) => VALID_URL_REGEX.test(token));
+
+		if (!detectedUrl) {
+			return undefined
+		}
+
+		let response = await axios.get(detectedUrl!, opts.fetchOpts);
+		response.url = detectedUrl;
+		
+		const info = getPreviewFromContent(response);
+
+			/*
 		const info = await getLinkPreview(previewLink, {
 			...opts.fetchOpts,
 			followRedirects: 'follow',
@@ -74,7 +128,9 @@ export const getUrlInfo = async(
 				}
 			},
 			headers: opts.fetchOpts as {}
-		})
+		})*/
+
+		const
 		if(info && 'title' in info && info.title) {
 			const [image] = info.images
 
