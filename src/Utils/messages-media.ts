cb4da2562203d9a4362d8187f3321b4b56849984
Nick (M2) <nicktee89@gmail.com>
2025-10-25 11:23:01 +0800

    testing

--- src/Utils/messages-media.ts
+++ src/Utils/messages-media.ts
@@ -417,8 +417,10 @@ export const encryptedStream = async (
 	}
 
 	try {
+		let i = 0;
+		let now = Date.now();
 		for await (const data of stream) {
-			logger?.debug('encrypted data loop start')
+			i++;
 			fileLength += data.length
 
 			if (
@@ -433,41 +435,31 @@ export const encryptedStream = async (
 
 			if (originalFileStream) {
 				if (!originalFileStream.write(data)) {
-					logger?.debug('encrypted data loop drain -1')
 					await once(originalFileStream, 'drain')
-					logger?.debug('encrypted data loop drain +1')
 				}
 			}
 
-			logger?.debug('encrypted data loop sha256Plain.update')
 			sha256Plain.update(data)
-			logger?.debug('encrypted data loop onChunk')
 			onChunk(aes.update(data))
-			logger?.debug('encrypted data loop end')
 		}
 
-		logger?.debug('encrypted data aes.final')
+		let end = Date.now();
+		console.log('totalEncryptedStream', i, end - now);
 		onChunk(aes.final())
-		logger?.debug('encrypted data aes.final end')
 
 		const mac = hmac.digest().slice(0, 10)
-		logger?.debug('encrypted data hmac.digest')
 		sha256Enc.update(mac)
-		logger?.debug('encrypted data sha256Enc.update')
 
 		const fileSha256 = sha256Plain.digest()
-		logger?.debug('encrypted data sha256Plain.digest')
 		const fileEncSha256 = sha256Enc.digest()
-		logger?.debug('encrypted data sha256Enc.digest')
 
 		encFileWriteStream.write(mac)
-		logger?.debug('encrypted data mac')
-
 		encFileWriteStream.end()
-		logger?.debug('encrypted data end')
 		originalFileStream?.end?.()
 		stream.destroy()
 
+		let end2 = Date.now();
+		console.log('last', i, end2 - now);
 		logger?.debug('encrypted data successfully')
 
 		return {
